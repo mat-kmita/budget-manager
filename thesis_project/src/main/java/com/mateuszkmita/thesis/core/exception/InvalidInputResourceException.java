1feff17af98eb864ed0cdc16c555183ce20f7088
Mateusz Kmita <mateusz.kmita@mail.com>
2021-11-28 17:34:01 +0100

    refactor: throw exceptions in transaction service

---
+++ thesis_project/src/main/java/com/mateuszkmita/thesis/core/exception/InvalidInputResourceException.java
@@ -0,0 +1,7 @@
+package com.mateuszkmita.thesis.core.exception;
+
+public class InvalidInputResourceException extends RuntimeException {
+    public InvalidInputResourceException(String s) {
+        super(s);
+    }
+}
