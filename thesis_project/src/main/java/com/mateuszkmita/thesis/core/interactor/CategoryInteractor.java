c8e363932ec4c7c25fd812723b490e1a1ae0892d
Mateusz Kmita <mateusz.kmita@mail.com>
2021-12-09 10:41:16 +0100

    feat: implement repository and service for categories

---
+++ thesis_project/src/main/java/com/mateuszkmita/thesis/core/interactor/CategoryInteractor.java
@@ -0,0 +1,55 @@
+package com.mateuszkmita.thesis.core.interactor;
+
+import com.mateuszkmita.thesis.core.exception.ResourceNotFoundException;
+import com.mateuszkmita.thesis.core.service.CategoryServiceInterface;
+import com.mateuszkmita.thesis.external.repository.CategoryRepositoryInterface;
+import com.mateuszkmita.thesis.model.Category;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryInteractor implements CategoryServiceInterface {
+
+    private final CategoryRepositoryInterface categoryRepository;
+
+    @Override
+    public Iterable<Category> findAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    @Override
+    public Optional<Category> findCategoryByName(String name) {
+        return categoryRepository.findByName(name);
+    }
+
+    @Override
+    public Category saveCategoryEntity(Category entity) {
+        if (entity.getId() != null) {
+            throw new IllegalArgumentException("New category must not have an ID!");
+        }
+
+        return categoryRepository.save(entity);
+    }
+
+    @Override
+    public Category updateCategoryEntity(Category updatedEntity) {
+        if (updatedEntity.getId() == null) {
+            throw new IllegalArgumentException("Updated category must have an ID!");
+        }
+
+        return categoryRepository.save(updatedEntity);
+    }
+
+    @Override
+    public void deleteCategoryById(int id) throws ResourceNotFoundException {
+        Optional<Category> optionalCategory = categoryRepository.findById(id);
+        if (optionalCategory.isEmpty()) {
+            throw new ResourceNotFoundException("category", id);
+        }
+
+        categoryRepository.delete(optionalCategory.get());
+    }
+}
