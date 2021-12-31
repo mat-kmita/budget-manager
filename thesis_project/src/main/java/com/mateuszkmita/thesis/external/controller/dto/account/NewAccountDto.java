a84346e3d798096b6c83c2083b2b5ffb25dcecaa
Mateusz Kmita <mateusz.kmita@mail.com>
2021-12-31 17:23:29 +0100

    refactor: allow today's date in money operations

---
+++ thesis_project/src/main/java/com/mateuszkmita/thesis/external/controller/dto/account/NewAccountDto.java
@@ -0,0 +1,2 @@
+package com.mateuszkmita.thesis.external.controller.dto.account;public class NewAccountDto {
+}
--- thesis_project/src/main/java/com/mateuszkmita/thesis/external/controller/dto/transaction/TransactionUpdateDto.java
+++ thesis_project/src/main/java/com/mateuszkmita/thesis/external/controller/dto/transaction/TransactionUpdateDto.java
@@ -3,8 +3,9 @@ package com.mateuszkmita.thesis.external.controller.dto.transaction;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import javax.validation.constraints.Past;
+import javax.validation.constraints.PastOrPresent;
 import java.time.LocalDate;
 
 public record TransactionUpdateDto(
-        @JsonFormat(pattern = "dd-MM-yyyy") @Past(message = "Future transactions are not supported!") LocalDate date,
+        @JsonFormat(pattern = "dd-MM-yyyy") @PastOrPresent(message = "Future transactions are not supported!") LocalDate date,
         String memo, Integer amount, String payee, Integer categoryId) {}
